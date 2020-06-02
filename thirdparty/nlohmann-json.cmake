d9cc7aa3132c5c186b6edd63fb305ac016ff5b3b
Maxim Menshikov <maxim@menshikov.org>
2020-06-02 15:45:47 +0300

    Set JSON build parameters more correctly with respect to FetchContent

--- thirdparty/nlohmann-json.cmake
+++ thirdparty/nlohmann-json.cmake
@@ -3,11 +3,15 @@ cmake_minimum_required(VERSION 3.14 FATAL_ERROR)
 
 include(FetchContent)
 
+set(BUILD_TESTING OFF)
+set(JSON_BuildTests OFF)
+set(JSON_MultipleHeaders ON)
+
 FetchContent_Declare(
   nlohmann_json
   GIT_REPOSITORY 	"https://github.com/maximmenshikov/json"
   GIT_TAG 			"develop"
-  CMAKE_ARGS 		-DJSON_BuildTests=OFF -DJSON_MultipleHeaders=ON -GNinja
+  CMAKE_ARGS 		-DBUILD_TESTING=OFF -DJSON_BuildTests=OFF -DJSON_MultipleHeaders=ON -GNinja
 )
 
 FetchContent_MakeAvailable(nlohmann_json)
